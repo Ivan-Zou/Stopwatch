9c62163f1138a39d56381a81768f656df7108a0f
Ivan Zou <ivanzou2@gmail.com>
2022-06-01 11:41:40 -0400

    made JFrame, JPanel, JLabel, and JButton final

--- src/stopwatch/RunStopwatch.java
+++ src/stopwatch/RunStopwatch.java
@@ -13,21 +13,21 @@ import java.awt.Font;
 public class RunStopwatch implements Runnable {
     @Override
     public void run() {
-        JFrame frame = new JFrame("Stopwatch");
+        final JFrame frame = new JFrame("Stopwatch");
         frame.setLocation(Constants.WINDOW_LOCATION_X, Constants.WINDOW_LOCATION_Y);
         frame.setPreferredSize(new Dimension(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT));
 
-        JPanel timePanel = new JPanel();
+        final JPanel timePanel = new JPanel();
         frame.add(timePanel, BorderLayout.CENTER);
-        JLabel stopwatchTime = new JLabel("00:00:00.00");
+        final JLabel stopwatchTime = new JLabel("00:00:00.00");
         stopwatchTime.setFont(new Font(null, Font.PLAIN, Constants.TIME_FONT_SIZE));
-        StopwatchMechanics stopwatch = new StopwatchMechanics(stopwatchTime);
+        final StopwatchMechanics stopwatch = new StopwatchMechanics(stopwatchTime);
         timePanel.add(stopwatchTime);
 
-        JPanel controlPanel = new JPanel();
+        final JPanel controlPanel = new JPanel();
         frame.add(controlPanel, BorderLayout.SOUTH);
 
-        JButton reset = new JButton("           ");
+        final JButton reset = new JButton("           ");
         reset.setFocusable(false);
         reset.addActionListener(actionEvent -> {
             if (stopwatch.isStopped()) {
@@ -40,7 +40,7 @@ public class RunStopwatch implements Runnable {
         // Spacer
         controlPanel.add(Box.createRigidArea(new Dimension(Constants.SPACER_WIDTH, 0)));
 
-        JButton startStop = new JButton("Start");
+        final JButton startStop = new JButton("Start");
         startStop.setFocusable(false);
         startStop.addActionListener(actionEvent -> {
             if (stopwatch.isReset() || stopwatch.isStopped()) {
